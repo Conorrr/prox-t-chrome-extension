286dfd539b861cf7c9df20ff976aaad28b6b3fc1
admin <admin@Conors-MacBook-Pro.local>
2016-04-11 19:11:11 +0100

    initial

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var requestPattern = "http://usher\.ttvnw\.net/api/channel/hls/[a-zA-Z0-9_]{4,25}\.m3u8";
+
+var getPair = function(str) {
+	return str.split("=");
+}
+
+var buildUrl = function(steamerName, params) {
+	return "http://162.243.99.47/usher.ttvnw.net/api/channel/hls/" + steamerName + ".m3u8?" + buildParamString(params);
+}
+
+var buildParamString = function(params) {
+	var out = [];
+	console.log(params);
+	for (key in params) {
+		out.push(key + "=" + params[key]);
+	};
+	return out.join("&");
+}
+
+// redirect request
+var callback = function(request) {
+
+	if (!request.url.match(requestPattern)) {
+		return;
+	}
+	var parts = request.url.split("&");
+
+	var nameAndToken = parts[0].substring(parts[0].lastIndexOf("/") + 1, parts[0].length).split("?");
+	// es6 [name, token] = nameAndToken ??
+	var name = nameAndToken[0].slice(0, -5);
+	var props = {
+		"token": nameAndToken[1].substring(6)
+	};
+
+	// extract parts
+	for (i = 1; i < parts.length; i++) {
+		var keyPair = getPair(parts[i]);
+		props[keyPair[0]] = keyPair[1];
+	}
+
+	var blockingResponse = {
+		"redirectUrl": buildUrl(name, props)
+	};
+
+	return blockingResponse;
+
+}
+
+var filter = {
+	urls: ["<all_urls>"]
+};
+var opt_extraInfoSpec = ["blocking"];
+
+chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
