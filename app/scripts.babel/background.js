5779d6c9d2432210be125e38c083e7ea713a7f2f
Conor Restall <conorrr@users.noreply.github.com>
2016-04-14 17:46:04 +0100

    restructure everything, tabs now have unique settings & sessions

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,122 +1,157 @@
 'use strict';
 
-var requestPattern = 'http://usher\.ttvnw\.net/api/channel/hls/[a-zA-Z0-9_]{4,25}\.m3u8';
 
-var enabled = false;
-var servers = {
-	'London': 'lon.restall.io:5050',
-	'Miami': 'mia.restall.io:5050',
-	'Silicon Valley': 'svy.restall.io:5050',
-	'Sydney': 'syd.restall.io:5050',
-	'localhost': 'localhost:5050'
-};
+//once it's working change to let [getServers,asTab] = {...} // see IIFE
+let asTab = (() => {
+  let requestPattern = 'http://usher\.ttvnw\.net/api/channel/hls/[a-zA-Z0-9_]{4,25}\.m3u8';
 
-var server = 'London';
+  let defaultServer = 'London';
 
-var enable = function() {
-	enabled = true;
-	restartCurrentTwitchTab();
-}
+  // {tabId:{enabled:t/f, server:'London'}, ...}
+  let status = {};
 
-var disable = function() {
-	enabled = false;
-	restartCurrentTwitchTab();
-}
+  let servers = {
+    'London': 'lon.restall.io:5050',
+    'Miami': 'mia.restall.io:5050',
+    'Silicon Valley': 'svy.restall.io:5050',
+    'Sydney': 'syd.restall.io:5050',
+    'localhost': 'localhost:5050'
+  };
 
-var getStatus = function() {
-	return enabled;
-}
+  let methods = function(tabId) {
+    return {
+      enable: () => {
+        status[tabId].enabled = true;
+        restartTwitchVideo(tabId);
+      },
 
-var getServers = function() {
-	return servers;
-}
+      disable: () => {
+        status[tabId].enabled = false;
+        restartTwitchVideo(tabId);
+      },
 
-var getCurrentServer = function() {
-	return server;
-}
+      getStatus: () => {
+        return status[tabId].enabled;
+      },
 
-var setServer = function(newServer) {
-	server = newServer;
+      getCurrentServer: () => {
+        return status[tabId].server;
+      },
 
-	if (enabled) {
-		restartCurrentTwitchTab();
-	}
-}
+      setServer: (newServer) => {
+        status[tabId].server = newServer;
+
+        if (enabled) {
+          restartTwitchVideo(tabId);
+        }
+      },
+      closeTab: () => {
+
+      },
+      getServers: () => {
+        // immutable.of()
+        return servers;
+      }
+    }
+  };
+
+  let asTab = (tabId) => {
+    if (!status[tabId]) {
+      status[tabId] = {
+        enabled: false,
+        server: defaultServer
+      };
+    }
+
+    return methods(tabId);
+  }
+
+  // Private internals
+
+  let getPair = function(str) {
+    return str.split('=');
+  }
+
+  let buildUrl = function(server, steamerName, params) {
+    return 'http://' + servers[server] + '/init/' + steamerName + '?' + buildParamString(params);
+  }
+
+  let buildParamString = function(params) {
+    let out = [];
+    for (let key in params) {
+      out.push(key + '=' + params[key]);
+    };
+    return out.join('&');
+  }
 
 
-// Private internals
 
-var getPair = function(str) {
-	return str.split('=');
-}
+  let restartCurrentTwitchTab = function() {
+    chrome.tabs.query({
+      active: true
+    }, function(tabs) {
+      // problem with mutliple windows -> if there are multiple windows there are multiple active tabs
+      restartTwitchVideo(tabs[0].id);
+    });
+  }
 
-var buildUrl = function(steamerName, params) {
-	return 'http://' + servers[server] + '/init/' + steamerName + '?' + buildParamString(params);
-}
+  let restartTwitchVideo = function(tabId) {
+    // this is really hacky, once i get time i will replace this with a nicer way to restart videos
+    let code = 'document.getElementsByClassName(\'player-button--playpause\')[0].click();setTimeout(function(){ document.getElementsByClassName(\'player-button--playpause\')[0].click(); }, 1000);';
+    chrome.tabs.executeScript(tabId, {
+      code: code
+    }, function() {});
+  }
 
-var buildParamString = function(params) {
-	var out = [];
-	for (var key in params) {
-		out.push(key + '=' + params[key]);
-	};
-	return out.join('&');
-}
 
-// redirect request
-var callback = function(request) {
-	if (!enabled) {
-		return;
-	}
+  // redirect request
+  let callback = function(request) {
+    let tabId = request.tabId;
 
-	if (!request.url.match(requestPattern)) {
-		return;
-	}
+    if (!status[tabId].enabled) {
+      return;
+    }
 
-	var parts = request.url.split('&');
+    if (!request.url.match(requestPattern)) {
+      return;
+    }
 
-	var nameAndToken = parts[0].substring(parts[0].lastIndexOf('/') + 1).split('?');
-	var name = nameAndToken[0].slice(0, -5);
-	var props = {
-		'token': nameAndToken[1].substring(6)
-	};
+    let parts = request.url.split('&');
 
-	// extract parts
-	for (var i = 1; i < parts.length; i++) {
-		var keyPair = getPair(parts[i]);
-		props[keyPair[0]] = keyPair[1];
-	}
+    let nameAndToken = parts[0].substring(parts[0].lastIndexOf('/') + 1).split('?');
+    let name = nameAndToken[0].slice(0, -5);
+    let props = {
+      'token': nameAndToken[1].substring(6)
+    };
 
-	var blockingResponse = {
-		'redirectUrl': buildUrl(name, props)
-	};
+    // extract parts
+    for (let i = 1; i < parts.length; i++) {
+      let keyPair = getPair(parts[i]);
+      props[keyPair[0]] = keyPair[1];
+    }
 
-	return blockingResponse;
-}
+    let server = status[tabId].server;
 
-var restartCurrentTwitchTab = function() {
-	chrome.tabs.query({
-		active: true
-	}, function(tabs) {
-		restartTwitchVideo(tabs[0].id);
-	});
-}
+    let blockingResponse = {
+      'redirectUrl': buildUrl(server, name, props)
+    };
 
-var restartTwitchVideo = function(tabId) {
-	var code = 'document.getElementsByClassName(\'player-button--playpause\')[0].click();setTimeout(function(){ document.getElementsByClassName(\'player-button--playpause\')[0].click(); }, 1000);';
-	chrome.tabs.executeScript(tabId, {
-		code: code
-	}, function() {});
-}
+    return blockingResponse;
+  }
 
-var filter = {
-	urls: ['http://usher.ttvnw.net/api/channel/hls/*']
-};
-var opt_extraInfoSpec = ['blocking'];
+  let filter = {
+    urls: ['http://usher.ttvnw.net/api/channel/hls/*']
+  };
 
-chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
+  let opt_extraInfoSpec = ['blocking'];
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (tab.url.indexOf('https://www.twitch.tv/') == 0) {
-		chrome.pageAction.show(tabId);
-	}
-});
+  chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
+
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url.indexOf('https://www.twitch.tv/') == 0 && changeInfo.status == 'complete') {
+      chrome.pageAction.show(tabId);
+    }
+  });
+
+  return asTab
+})();
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,48 +1,54 @@
 'use strict';
 
-var bg = chrome.extension.getBackgroundPage();
+chrome.tabs.query({
+	active: true,
+	lastFocusedWindow: true
+}, function(tabs) {
+	// Do something with tabs
+	var bg = chrome.extension.getBackgroundPage().asTab(tabs[0].id);
 
-var redraw = function() {
-	document.getElementById('status').innerHTML = (bg.getStatus() ? 'enabled' : 'disabled');
-	document.getElementById('enableButton').text = (bg.getStatus() ? 'disable' : 'enable');
-}
-
-var initialRender = function() {
-	var serversSelect = document.getElementById('servers');
-	var servers = bg.getServers();
-	var currentServer = bg.getCurrentServer();
-	serversSelect.appendChild(generateOption('Select a region', false));
-	for (var key in servers) {
-		var selected = key == currentServer;
-		serversSelect.appendChild(generateOption(key, selected));
+	var redraw = function() {
+		document.getElementById('status').innerHTML = (bg.getStatus() ? 'enabled' : 'disabled');
+		document.getElementById('enableButton').text = (bg.getStatus() ? 'disable' : 'enable');
 	}
-}
 
-var generateOption = function(name, selected) {
-	var option = document.createElement('option');
-	option.text = name;
-	option.value = name;
-	if (selected) {
-		option.selected = 'selected';
+	var initialRender = function() {
+		var serversSelect = document.getElementById('servers');
+		var servers = bg.getServers();
+		var currentServer = bg.getCurrentServer();
+		serversSelect.appendChild(generateOption('Select a region', false));
+		for (var key in servers) {
+			var selected = key == currentServer;
+			serversSelect.appendChild(generateOption(key, selected));
+		}
 	}
-	return option;
-}
 
-var statusSelect = document.getElementById('servers');
+	var generateOption = function(name, selected) {
+		var option = document.createElement('option');
+		option.text = name;
+		option.value = name;
+		if (selected) {
+			option.selected = 'selected';
+		}
+		return option;
+	}
 
-statusSelect.addEventListener('change', function() {
-	bg.setServer(statusSelect.value);
+	var statusSelect = document.getElementById('servers');
+
+	statusSelect.addEventListener('change', function() {
+		bg.setServer(statusSelect.value);
+	});
+
+	document.getElementById('enableButton').addEventListener('click', function() {
+		if (bg.getStatus()) {
+			bg.disable();
+			redraw();
+		} else {
+			bg.enable();
+			redraw();
+		}
+	});
+
+	initialRender();
+	redraw();
 });
-
-document.getElementById('enableButton').addEventListener('click', function() {
-	if (bg.getStatus()) {
-		bg.disable();
-		redraw();
-	} else {
-		bg.enable();		
-		redraw();
-	}
-});
-
-initialRender();
-redraw();
